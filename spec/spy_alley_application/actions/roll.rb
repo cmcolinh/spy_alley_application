5b5e43f0b10d46587a9c7e9195bf13b25b16090a
colin.a.horner <HORNER_COLIN@token.co.jp>
2019-08-28 12:56:14 +0900

    [Add] Actions#roll tests added

--- spec/spy_alley_application/actions/roll.rb
+++ spec/spy_alley_application/actions/roll.rb
@@ -17,24 +17,41 @@ end
 
 include SpyAlleyApplication::Actions
 
-Rspec.describe SpyAlleyApplication::Actions do
-  let(:player, ->{Class.new{
-    def game
-      1
-    end
+RSpec.describe SpyAlleyApplication::Actions do
+  let(:player) {
+    Class.new{
+      def game
+        1
+      end
 
-    def seat
-      1
-    end  
-  }.new)
-  let(:change_orders, ->{Class.new{
-    def add_die_roll(player:, rolled:)
-    end
-  }.new)
-  let(:roll_die, ->{->{1}})
+      def seat
+        1
+      end  
+    }.new
+  }
+  let(:change_orders) {
+    Class.new{
+      def initialize
+        @called = {}
+      end
+      def add_die_roll(player:, rolled:)
+        @called[:add_die_roll] = ((@called[:add_die_roll] || 0) + 1)
+      end
+      def called
+        @called.dup
+      end
+    }.new
+  }
+  let(:roll_die, &->{->{1}})
+  let(:calling_method) {->{roll(player_model: player, change_orders: change_orders, roll_die: roll_die)}}
   describe '#roll' do
     it 'returns the number rolled' do
-      expect roll(player: player, change_orders: change_orders, roll_die: roll_die).to eql(1)
+      expect(calling_method.()).to eql(1)
+    end
+
+    it 'calls change_orders' do
+      roll(player_model: player, change_orders: change_orders, roll_die: roll_die)
+      expect(change_orders.called[:add_die_roll]).to eql(1)
     end
   end
 end
