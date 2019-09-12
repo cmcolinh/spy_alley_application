5bfbc76f7b69c5d4f8fcd2f14077b0566262244e
colin <H2597@token.co.jp>
2019-09-13 08:09:02 +0900

    continuting to add SpyAlleyApplication::Validator::RollNoMoveCardValidator specs

--- spec/spy_alley_application/validator/roll_no_move_validator_spec.rb
+++ spec/spy_alley_application/validator/roll_no_move_validator_spec.rb
@@ -1,27 +1,102 @@
 # frozen_string_literal: true
 
 RSpec.describe SpyAlleyApplication::Validator::RollNoMoveCardValidator do
-  let(:validate){
-    SpyAlleyApplication::Validator::RollNoMoveCardValidator::new(
-      roll_options:            nil,
-      accusation_targets:      ['seat_2', 'seat_3'],
-      accusable_nationalities: %w(french german spanish italian american russian)
-    )
-  }
   describe '#call' do
-    describe 'choosing to roll' do
-      it 'will validate correctly when simply choosing to roll' do
-        expect(validate.(player_action: 'roll')).to be_success
+    describe 'non-admin' do
+      let(:validate){
+        SpyAlleyApplication::Validator::RollNoMoveCardValidator::new(
+          roll_options:            nil,
+          accusation_targets:      ['seat_2', 'seat_3'],
+          accusable_nationalities: %w(french german spanish italian american russian)
+        )
+      }
+      describe 'choosing to roll' do
+        it 'will validate correctly when simply choosing to roll' do
+          expect(validate.(player_action: 'roll')).to be_success
+        end
+
+        it 'will fail if you choose to roll, and then choose a target for an accusation' do
+          expect(
+            validate.(
+              player_action: 'roll',
+              player_to_accuse: 'seat_2',
+              nationality: 'french'
+            )
+          ).to be_failure
+        end
+
+        it 'will fail if you choose to roll, and player_to_accuse is set' do
+          expect(validate.(player_action: 'roll', player_to_accuse: 'seat_2')).to be_failure
+        end
+
+        it 'will fail if you choose to roll, and nationality is set' do
+          expect(validate.(player_action: 'roll', nationality: 'french')).to be_failure
+        end
+
+        it 'will fail if you choose to roll and try to set the result without being an admin' do
+          expect(validate.(player_action: 'roll', choose_result: 5)).to be_failure
+        end
       end
 
-      it 'will fail if you choose to roll, and then choose a target for an accusation' do
-        expect(
-          validate.(
-            player_action: 'roll',
-            player_to_accuse: 'seat_2',
-            nationality: 'french'
-          )
-        ).to be_failure
+      describe 'choosing to make accusation' do
+        it 'will successfully validate an accusation with valid seats and nationalities' do
+          expect(
+            validate.(
+              player_action:    'make_accusation',
+              player_to_accuse: 'seat_2',
+              nationality:      'french'
+            )
+          ).to be_success
+        end
+
+        it 'will fail if you choose an invalid player' do
+          expect(
+            validate.(
+              player_action:    'make_accusation',
+              player_to_accuse: 'invalid_seat',
+              nationality:      'french'
+            )
+          ).to be_failure
+        end
+
+        it 'will fail if you choose an invalid nationality' do
+          expect(
+            validate.(
+              player_action:    'make_accusation',
+              player_to_accuse: 'seat_2',
+              nationality:      'invalid_nationality'
+            )
+          ).to be_failure
+        end
+
+        it 'will fail if you do not select a player to accuse' do
+          expect(
+            validate.(
+              player_action:    'make_accusation',
+              nationality:      'french'
+            )
+          ).to be_failure
+        end
+
+        it 'will fail if you do not select a nationality to accuse the player of being' do
+          expect(
+            validate.(
+              player_action:    'make_accusation',
+              player_to_accuse: 'french'
+            )
+          ).to be_failure
+        end
+
+        it 'will fail if you attempt to choose a result while making an accusation' do
+          expect(
+            validate.(
+              player_action:    'make_accusation',
+              player_to_accuse: 'seat_2',
+              nationality:      'french',
+              choose_result:    5
+            )
+          ).to be_failure
+        end
       end
     end
   end
