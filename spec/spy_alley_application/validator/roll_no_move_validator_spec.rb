2abe78237d1f831c55e159c33474baed157c8130
colin.a.horner <HORNER_COLIN@token.co.jp>
2019-09-12 12:58:09 +0900

    Adding RollNoMoveValidator specs

--- spec/spy_alley_application/validator/roll_no_move_validator_spec.rb
+++ spec/spy_alley_application/validator/roll_no_move_validator_spec.rb
@@ -1,11 +1,28 @@
 # frozen_string_literal: true
 
 RSpec.describe SpyAlleyApplication::Validator::RollNoMoveCardValidator do
-  let(:validator){
+  let(:validate){
     SpyAlleyApplication::Validator::RollNoMoveCardValidator::new(
-      roll_options:         nil,
-      accusation_targets:   ['seat_2', 'seat_3'],
-      accusable_identities: %w(french german spanish italian american russian)
+      roll_options:            nil,
+      accusation_targets:      ['seat_2', 'seat_3'],
+      accusable_nationalities: %w(french german spanish italian american russian)
     )
   }
+  describe '#call' do
+    describe 'choosing to roll' do
+      it 'will validate correctly when simply choosing to roll' do
+        expect(validate.(player_action: 'roll')).to be_success
+      end
+
+      it 'will fail if you choose to roll, and then choose a target for an accusation' do
+        expect(
+          validate.(
+            player_action: 'roll',
+            player_to_accuse: 'seat_2',
+            nationality: 'french'
+          )
+        ).to be_failure
+      end
+    end
+  end
 end
