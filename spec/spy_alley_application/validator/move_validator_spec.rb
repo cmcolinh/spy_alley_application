95cc446e9de68f3bebd83cd9751cb10ce72ef750
colin <H2597@token.co.jp>
2019-09-17 23:45:01 +0900

    Finish adding MoveValidator specs

---
+++ spec/spy_alley_application/validator/move_validator_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+RSpec.describe SpyAlleyApplication::Validator::MoveValidator do
+  describe '#call' do
+    let(:validate){
+      SpyAlleyApplication::Validator::MoveValidator::new(
+        space_options: ['18', '2s']
+      )
+    }
+    it 'will validate correctly when simply choosing to pass' do
+      expect(validate.(player_action: 'move', space: '18')).to be_success
+    end
+
+    it "will fail if you choose any action other than 'move'" do
+      expect(validate.(player_action: 'invalid', space: '18')).to be_failure
+    end
+
+    it 'will fail if you choose an invalid space to move' do
+      expect(validate.(player_action: 'move', space: '20')).to be_failure
+    end
+
+    it 'will fail if you fail to specify a space to move' do
+      expect(validate.(player_action: 'move')).to be_failure
+    end
+
+    it 'will fail if you specify multiple spaces to move' do
+      expect(validate.(player_action: 'move', space: ['18', '2s'])).to be_failure
+    end
+  end
+end
