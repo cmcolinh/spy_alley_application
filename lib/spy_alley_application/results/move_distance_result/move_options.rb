ffc124804a32026fb8adda8e3e5df58b655af5df
colin.a.horner <HORNER_COLIN@token.co.jp>
2019-10-01 12:57:42 +0900

    Finish adding move options specs and implementation

--- lib/spy_alley_application/results/move_distance_result/move_options.rb
+++ lib/spy_alley_application/results/move_distance_result/move_options.rb
@@ -4,8 +4,19 @@ module SpyAlleyApplication
   module Results
     class MoveDistanceResult
       class MoveOptions
+        @@board_nodes = (
+          ((0..13).to_a + (14..22).to_a).map{|space| ["#{space}", "#{space + 1}"]} +
+          (1..8).map{|space| ["#{space}s", "#{space + 1}s"]} +
+          [['14', ['15', '1s']], ['9s', '22'], ['23', '0'], ['s14', '1s']]
+        ).to_h
+
         def call(location:, move_distance:)
-          []
+          location = 's14' if location.eql?('14')
+          move_spaces = Array(location)
+          move_distance.times do
+            move_spaces = Array(move_spaces.map{|space| @@board_nodes[space]}).flatten
+          end
+          move_spaces
         end
       end
     end
--- spec/spy_alley_application/results/move_distance_result/move_options_spec.rb
+++ spec/spy_alley_application/results/move_distance_result/move_options_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe SpyAlleyApplication::Results::MoveDistanceResult::MoveOptions do
         (1..6).each do |distance|
           it "correctly moves to space '#{location + distance}' when moving #{distance} spaces" do
             expect(
-              move_options_for.(location: location, move_distance: distance)
+              move_options_for.(location: "#{location}", move_distance: distance)
             ).to match_array(["#{location + distance}"])
           end
         end
@@ -25,7 +25,7 @@ RSpec.describe SpyAlleyApplication::Results::MoveDistanceResult::MoveOptions do
         (1..6).each do |distance|
           it "correctly moves to space '#{location + distance}s' when moving #{distance} spaces" do
             expect(
-              move_options_for.(location: location, move_distance: distance)
+              move_options_for.(location: "#{location}s", move_distance: distance)
             ).to match_array(["#{location + distance}s"])
           end
         end
@@ -49,7 +49,7 @@ RSpec.describe SpyAlleyApplication::Results::MoveDistanceResult::MoveOptions do
         (1..(14 - location)).each do |distance|
           it "correctly gives one option at location '#{location + distance}' when moving #{distance} spaces" do
             expect(
-              move_options_for.(location: '#{location}', move_distance: distance)
+              move_options_for.(location: "#{location}", move_distance: distance)
             ).to match_array(["#{location + distance}"])
           end
         end
@@ -58,10 +58,120 @@ RSpec.describe SpyAlleyApplication::Results::MoveDistanceResult::MoveOptions do
             "and one at '#{location + distance - 14}s' when moving #{distance} spaces" do
 
             expect(
-              move_options_for.(location: '#{location}', move_distance: distance)
+              move_options_for.(location: "#{location}", move_distance: distance)
             ).to match_array(["#{location + distance}", "#{location + distance - 14}s"])
           end
         end
+      end
+    end
+  end
+  describe "from location '23' the player will complete a lap" do
+    (1..6).each do |distance|
+      it "correctly moves to space '#{distance - 1}' when moving #{distance} spaces" do
+        expect(
+          move_options_for.(location: '23', move_distance: distance)
+        ).to match_array(["#{distance - 1}"])
+      end
+    end
+  end
+  describe "between spaces '18' and '22' there is a chance that the move distance will pass start" do
+    (18..22).to_a.each do |location|
+      describe "from location '#{location}'" do
+        (1..(23 - location)).each do |distance|
+          it "correctly goes to location '#{location + distance}' " +
+            "when moving #{distance} spaces, staying on the same lap" do
+
+            expect(
+              move_options_for.(location: "#{location}", move_distance: distance)
+            ).to match_array(["#{location + distance}"])
+          end
+        end
+        ((24 - location)..6).each do |distance|
+          it "correctly goes to location '#{(location + distance) % 24}' " +
+            "when moving #{distance} spaces, starting a new lap" do
+
+            expect(
+              move_options_for.(location: "#{location}", move_distance: distance)
+            ).to match_array(["#{(location + distance) % 24}"])
+          end
+        end
+      end
+    end
+  end
+  describe "from locations '4s' and '5s' there is a chance to leave spy alley, but no chance to complete a lap" do
+    (4..5).each do |location|
+      describe "from location '#{location}'s" do
+        (1..(9 - location)).each do |distance|
+          it "correctly goes to location '#{location + distance}s' " +
+            "when moving #{distance} spaces, staying in spy alley" do
+
+            expect(
+              move_options_for.(location: "#{location}s", move_distance: distance)
+            ).to match_array(["#{location + distance}s"])
+          end
+        end
+        ((10 - location)..6).each do |distance|
+          it "correctly goes to location '#{location + distance + 12}' " +
+            "when moving #{distance} spaces, leaving spy alley" do
+
+            expect(
+              move_options_for.(location: "#{location}s", move_distance: distance)
+            ).to match_array(["#{location + distance + 12}"])
+          end
+        end
+      end
+    end
+  end
+  describe "from locations '6s', '7s', and '8s' the player may or may not leave spy alley, and may complete a lap" do
+    (6..8).each do |location|
+      describe "from location '#{location}'s" do
+        (1..(9 - location)).each do |distance|
+          it "correctly goes to location '#{location + distance}s' " +
+            "when moving #{distance} spaces, staying in spy alley" do
+
+            expect(
+              move_options_for.(location: "#{location}s", move_distance: distance)
+            ).to match_array(["#{location + distance}s"])
+          end
+        end
+        ((10 - location)..(11 - location)).each do |distance|
+          it "correctly goes to location '#{location + distance + 12}' " +
+            "when moving #{distance} spaces, leaving spy alley, and staying on the same lap" do
+
+            expect(
+              move_options_for.(location: "#{location}s", move_distance: distance)
+            ).to match_array(["#{location + distance + 12}"])
+          end
+        end
+        ((12 - location)..6).each do |distance|
+          it "correctly goes to location '#{(location + distance + 12) % 24}' " +
+            "when moving #{distance} spaces, leaving spy alley, and starting a new lap" do
+
+            expect(
+              move_options_for.(location: "#{location}s", move_distance: distance)
+            ).to match_array(["#{(location + distance + 12) % 24}"])
+          end
+        end
+      end
+    end
+  end
+  describe "from locations '9s' the player will leave spy alley, and may complete a lap" do
+    (1..2).each do |distance|
+      it "correctly goes to location '#{21 + distance}' " +
+        "when moving #{distance} spaces, staying on the same lap" do
+
+        expect(
+          move_options_for.(location: '9s', move_distance: distance)
+        ).to match_array(["#{21 + distance}"])
+      end
+    end
+    (3..6).each do |distance|
+      it "correctly goes to location '#{(21 + distance) % 24}' " +
+        "when moving #{distance} spaces, starting a new lap" do
+
+        expect(
+          move_options_for.(location: '9s', move_distance: distance)
+        ).to match_array(["#{(21 + distance) % 24}"])
       end
     end
   end
