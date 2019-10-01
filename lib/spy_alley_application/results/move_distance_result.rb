7f9b2f2c77b0b08a306d4e7b3f52492bf4ab72c5
colin <H2597@token.co.jp>
2019-10-02 08:09:48 +0900

    bugfix

--- lib/spy_alley_application/results/move_distance_result.rb
+++ lib/spy_alley_application/results/move_distance_result.rb
@@ -8,7 +8,7 @@ module SpyAlleyApplication
   module Results
     class MoveDistanceResult
       extend Dry::Initializer
-      option :move_options_from, default: ->{SpyApplication::Results::MoveDistanceResult::MoveOptions::new}
+      option :move_options_from, default: ->{SpyAlleyApplication::Results::MoveDistanceResult::MoveOptions::new}
       option :move_result, default: ->{SpyAlleyApplication::Results::MoveResult::new}
       def call(player_model:, change_orders:, action_hash:, move_distance:)
         move_options = move_options_from.(location: player_model.location, move_distance: move_distance)
