12c0de7855c7fd2a776822e88b94012d2c5a76b3
colin <H2597@token.co.jp>
2019-10-03 07:56:13 +0900

    Begin adding move distance result specs

--- lib/spy_alley_application/results/move_distance_result.rb
+++ lib/spy_alley_application/results/move_distance_result.rb
@@ -9,11 +9,26 @@ module SpyAlleyApplication
     class MoveDistanceResult
       extend Dry::Initializer
       option :move_options_from, default: ->{SpyAlleyApplication::Results::MoveDistanceResult::MoveOptions::new}
-      option :move_result, default: ->{SpyAlleyApplication::Results::MoveResult::new}
+      option :get_move_result, default: ->{SpyAlleyApplication::Results::MoveResult::new}
       def call(player_model:, change_orders:, action_hash:, move_distance:)
         move_options = move_options_from.(location: player_model.location, move_distance: move_distance)
-        #if move_distance.length > 1
+        if move_options.length > 1
+          SpyAlleyApplication::Results::MoveOptionResult::new(
+            player_model:  player_model,
+            change_orders: change_orders,
+            action_hash:   action_hash,
+            move_options:  move_options
+          )
+        elsif move_options.length.eql? 1
+          get_move_result.(
+            player_model:  player_model,
+            change_orders: change_orders,
+            action_hash:   action_hash,
+            space_to_move: move_options.first
+          )
+        end
       end
     end
   end
 end
+
---
+++ spec/spy_alley_application/results/move_distance_result_spec.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+RSpec.describe SpyAlleyApplication::Results::MoveResult::FinishedLap do
+end
+
