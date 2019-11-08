b62942838fef983175b588b6cd4759d5db2c752e
colin.a.horner <HORNER_COLIN@token.co.jp>
2019-11-08 12:58:19 +0900

    Adding sold top secret information implementation

--- lib/spy_alley_application/results/sold_top_secret_information.rb
+++ lib/spy_alley_application/results/sold_top_secret_information.rb
@@ -6,6 +6,14 @@ require 'spy_alley_application/results/sold_top_secret_information/twenty'
 module SpyAlleyApplication
   module Results
     class SoldTopSecretInformation
+      def call(player_model:, change_orders: money_earned)
+        change_orders.add_money_action(
+          player: {game: player_model.game, seat: player_model.seat},
+          amount: money_earned,
+          reason: 'for selling top secret information'
+        )
+        false # the current player's turn will *not* continue
+      end
     end
   end
 end
--- lib/spy_alley_application/results/sold_top_secret_information/twenty.rb
+++ lib/spy_alley_application/results/sold_top_secret_information/twenty.rb
@@ -1,9 +1,22 @@
 # frozen_string_literal: true
 
+require 'dry/initializer'
+
 module SpyAlleyApplication
   module Results
     class SoldTopSecretInformation
       class Twenty
+        extend Dry::Initializer
+        option :sold_top_secret_information, default: -> do
+          SpyAlleyApplication::Results::SoldTopSecretInformation::new
+        end
+        def call(player_model:, change_orders:, action_hash: nil, target_player_model: nil, decks_model: nil)
+          sold_top_secret_information.(
+            player_model: player_model,
+            change_orders: change_orders,
+            money_earned: 20
+          )
+        end
       end
     end
   end
