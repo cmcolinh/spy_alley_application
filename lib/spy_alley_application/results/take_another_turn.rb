bf980af304ffe337ea345169df7144e6efc18541
colin.a.horner <HORNER_COLIN@token.co.jp>
2019-11-12 12:46:52 +0900

    Add take another turnresult spec and implementation

--- lib/spy_alley_application/results/take_another_turn.rb
+++ lib/spy_alley_application/results/take_another_turn.rb
@@ -3,7 +3,9 @@
 module SpyAlleyApplication
   module Results
     class TakeAnotherTurn
+      def call(player_model:, change_orders:, action_hash:, target_player_model: nil, decks_model: nil)
+        true
+      end
     end
   end
 end
-
--- spec/spy_alley_application/results/take_another_turn_spec.rb
+++ spec/spy_alley_application/results/take_another_turn_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-RSpec.describe SpyAlleyApplication::Results::NoActionResult do
+RSpec.describe SpyAlleyApplication::Results::TakeAnotherTurn do
   let(:player_model, &->{PlayerMock::new})
   let(:change_orders, &->{ChangeOrdersMock::new})
   let(:action_hash) do
@@ -8,16 +8,16 @@ RSpec.describe SpyAlleyApplication::Results::NoActionResult do
       player_action: 'roll'
     }
   end
-  let(:no_result, &->{SpyAlleyApplication::Results::NoActionResult::new})
+  let(:no_result, &->{SpyAlleyApplication::Results::TakeAnotherTurn::new})
   describe '#call' do
-    it "returns false, indicating that it will not remain the same player's turn" do
+    it "returns true, indicating that it will remain the same player's turn" do
       expect(
         no_result.(
           player_model: player_model,
           change_orders: change_orders,
           action_hash: action_hash
         )
-      ).to be false
+      ).to be true
     end
   end
 end
