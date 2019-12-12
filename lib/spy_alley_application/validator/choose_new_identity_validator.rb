f3302d957504cad3ba92f3b9062944949a58f4e7
Colin Horner <HORNER.COLIN@token.co.jp>
2019-12-13 08:45:52 +0900

    Refactoring validators

--- lib/spy_alley_application/validator/choose_new_identity_validator.rb
+++ lib/spy_alley_application/validator/choose_new_identity_validator.rb
@@ -6,13 +6,24 @@ require 'dry/initializer'
 module SpyAlleyApplication
   class Validator
     class ChooseNewIdentityValidator < Dry::Validation::Contract
+      extend Dry::Initializer
       option :nationality_options
+      option :return_options
       params do
         required(:player_action).filled(:string, eql?: 'choose_spy_identity')
         required(:nationality).filled(:string)
+        required(:return_player).filled(:integer, included_in?: (1..6))
+        optional(:remaining_choices).filled(:array)
       end
       rule(:nationality) do
         key.failure({text: 'not a valid nationality', status: 422}) if !nationality_options.include?(values[:nationality])
+      end
+
+      def call(input)
+        input.reject!{|k, v| [:return_player, :remaining_choices].include?(k)}
+        input[:return_player] = return_options[:player]
+        input[:remaining_choices] = return_options[:choices] if return_options.has_key?(:choices)
+        super
       end
     end
   end
--- lib/spy_alley_application/validator/spy_eliminator_validator.rb
+++ lib/spy_alley_application/validator/spy_eliminator_validator.rb
@@ -13,6 +13,7 @@ module SpyAlleyApplication
         required(:player_action).filled(:string,  included_in?: legal_options)
         optional(:player_to_accuse).filled(:string)
         optional(:nationality).filled(:string)
+        required(:accusation_targets).filled(:array)
       end
       rule(:player_to_accuse) do
         key.failure({text: 'choosing a player to accuse not allowed unless you are making accusation', status: 400}) if !values[:player_action].eql?('make_accusation') && !values[:player_to_accuse].nil?
@@ -21,6 +22,11 @@ module SpyAlleyApplication
       rule(:nationality) do
         key.failure({text: 'choosing a nationality is not allowed unless either making an accusation or choosing a new spy identity', status: 400}) if !values[:player_action].eql?('make_accusation') && !values[:nationality].nil?
         key.failure({text: 'not a valid nationality', status: 422}) if values[:player_action].eql?('make_accusation') && !accusable_nationalities.include?(values[:nationality])
+      end
+
+      def call(input)
+        input[:accusation_targets] = accusable_nationalities
+        super
       end
     end
   end
