d56712555f912028086aeff89f3243fffc4b9a4e
Colin Horner <HORNER.COLIN@token.co.jp>
2019-12-25 08:24:08 +0900

    Working on fixing free_accusation

--- lib/spy_alley_application/actions/free_accusation_action.rb
+++ lib/spy_alley_application/actions/free_accusation_action.rb
@@ -9,32 +9,56 @@ module SpyAlleyApplication
     class FreeAccusationAction
       extend Dry::Initializer
       option :eliminate_player, default: ->{SpyAlleyApplication::Results::EliminatePlayerResult::new}
-      option :next_player_up_no_more_targets, default: ->{SpyAlleyApplication::Results::NextPlayerUp::new}
-      option :next_player_up_more_targets, default: -> do
-        SpyAlleyApplication::Results::NextPlayerUp::new
-      end
+      option :next_player_up_for, default: ->{SpyAlleyApplication::Results::NextPlayerUp::new}
       def call(change_orders:, player_model:, opponent_models:, action_hash:, decks_model: nil)
         target_player_model = get_target_player_model_from(opponent_models, action_hash)
+        accusation_targets = action_hash[:accusation_targets]
         guess_correct = action_hash[:nationality].eql?(target_player_model.spy_identity)
         change_orders = change_orders.add_action(action_hash.dup.reject{|k, v| k.eql?(:accusation_targets)})
-
+        return_player = nil
+        turn_complete = false
+        remaining_choices = accusation_targets - [action_hash[:player_to_accuse]]
         if guess_correct
           change_orders = change_orders.add_action(result: {guess_correct: true})
-          change_orders = eliminate_player.(
+          if remaining_choices.size > 0
+            return_player = player_model.seat
+          else
+            return_player = get_next_seat_from(player_model, (opponent_models - [target_player_model]))
+          end
+          options = {
             player_model:        player_model,
             target_player_model: target_player_model,
-            change_orders:       change_orders
-          )
+            change_orders:       change_orders,
+            return_player:       return_player
+          }
+          options[:remaining_choices] = remaining_choices.map{|s| s.gsub("seat_", "").to_i}
+          change_orders = eliminate_player.(options)
         else
           change_orders = change_orders.add_action(result: {guess_correct: false})
-          
+          if remaining_choices.size > 0
+            change_orders = change_orders.add_spy_eliminator_option(options: remaining_choices)
+          else
+            turn_complete = true
+          end
         end
-        change_orders
+        next_player_up_for.(
+          player_model: player_model,
+          opponent_models: opponent_models,
+          change_orders: change_orders,
+          action_hash: action_hash,
+          turn_complete?: turn_complete
+        )
       end
 
+      private
       def get_target_player_model_from(opponent_models, action_hash)
         seat = action_hash[:player_to_accuse].gsub('seat_', '').to_i
         opponent_models.select{|m| m.seat.eql? seat}.first
+      end
+
+      def get_next_seat_from(player_model, opponent_models)
+        opponent_seats = opponent_models.map(&:seat)
+        opponent_seats.select{|seat| seat > player_model.seat}.min || opponent_seats.min
       end
     end
   end
--- spec/spy_alley_application/actions/free_accusation_action_spec.rb
+++ spec/spy_alley_application/actions/free_accusation_action_spec.rb
@@ -2,7 +2,7 @@
 
 RSpec.describe SpyAlleyApplication::Actions::FreeAccusationAction do
   let(:player_model,    &->{PlayerMock::new})
-  let(:opponent_models, &->{[TargetPlayerMock::new]})
+  let(:opponent_models, &->{[PlayerMock::new(seat: 2), PlayerMock::new(seat: 3)]})
   let(:change_orders,   &->{ChangeOrdersMock::new})
   let(:eliminate_player) do 
     ->(options={}) do
@@ -17,7 +17,7 @@ RSpec.describe SpyAlleyApplication::Actions::FreeAccusationAction do
       SpyAlleyApplication::Actions::FreeAccusationAction::new(eliminate_player: eliminate_player)
     end
     let(:making_guess) do
-      ->(correct:) do
+      ->(correct:, more_options: false) do
         make_accusation.(
           player_model:    player_model,
           change_orders:   change_orders,
@@ -26,41 +26,49 @@ RSpec.describe SpyAlleyApplication::Actions::FreeAccusationAction do
             player_action:    'make_accusation',
             player_to_accuse: 'seat_2',
             nationality:      correct ? 'german' : 'spanish',
-            free_accusation:  true
-          }
+            free_accusation:  true,
+          }.tap{|h| h[:accusation_targets] = ['seat_3'] if more_options}
         )
       end
     end
     describe 'when guess is correct' do
-      it 'targets the target player for elimination' do
-        making_guess.(correct: true)
-        expect(eliminated_player[:seat]).to eql 2
-      end
+      describe 'there are more available guesses' do
+        it 'targets the target player for elimination' do
+          making_guess.(correct: true, more_options: true)
+          expect(eliminated_player[:seat]).to eql 2
+        end
 
-      it 'targets the guessing player as an eliminator' do
-        making_guess.(correct: true)
-        expect(eliminating_player[:seat]).to eql 1
-      end
+        it 'targets the guessing player as an eliminator' do
+          making_guess.(correct: true, more_options: true)
+          expect(eliminating_player[:seat]).to eql 1
+        end
 
-      it 'calls change_orders#add_action twice' do
-        making_guess.(correct: true)
-        expect(change_orders.times_called[:add_action]).to eql 2
+        it 'calls change_orders#add_action twice' do
+          making_guess.(correct: true, more_options: true)
+          expect(change_orders.times_called[:add_action]).to eql 2
+        end
+      end
+      describe 'there are no more available guesses' do
       end
     end
     describe 'when guess is incorrect' do
-      it 'does not target a player for elimination' do
-        making_guess.(correct: false)
-        expect(eliminated_player[:seat]).to be nil
-      end
+      describe 'there are more available guesses' do
+        it 'does not target a player for elimination' do
+          making_guess.(correct: false, more_options: true)
+          expect(eliminated_player[:seat]).to be nil
+        end
 
-      it 'does not target any player as an eliminator' do
-        making_guess.(correct: false)
-        expect(eliminating_player[:seat]).to be nil
-      end
+        it 'does not target any player as an eliminator' do
+          making_guess.(correct: false, more_options: true)
+          expect(eliminating_player[:seat]).to be nil
+        end
 
-      it 'calls change_orders#add_action twice' do
-        making_guess.(correct: false)
-        expect(change_orders.times_called[:add_action]).to eql 2
+        it 'calls change_orders#add_action twice' do
+          making_guess.(correct: false, more_options: true)
+          expect(change_orders.times_called[:add_action]).to eql 2
+        end
+      end
+      describe 'there are no more available guesses' do
       end
     end
   end
