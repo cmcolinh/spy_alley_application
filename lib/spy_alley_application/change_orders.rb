de3e19ce473610420a1365c4b6aebe72c45d68bb
Colin Horner <25807014+cmcolinh@users.noreply.github.com>
2020-03-15 21:50:58 +0900

    Add interface for change order nodes to accept visitors

--- lib/spy_alley_application/change_orders.rb
+++ lib/spy_alley_application/change_orders.rb
@@ -221,35 +221,56 @@ module SpyAlleyApplication
       extend Dry::Initializer
       option :player, type: Dry::Types['strict.hash']
       option :rolled, type: Dry::Types['strict.integer'].constrained(included_in: (1..6))
+      def accept visitor
+        visitor.process_die_roll self
+      end
     end
 
     class AdminDieRoll
       extend Dry::Initializer
       option :player,        type: Dry::Types['strict.hash']
       option :result_chosen, type: Dry::Types['strict.integer'].constrained(included_in: (1..6))
+      def accept visitor
+        visitor.process_admin_die_roll self
+      end
     end
 
     class AddMoveCard
       extend Dry::Initializer
       option :player,      type: Dry::Types['strict.hash']
       option :card_to_add, type: Dry::Types['strict.integer'].constrained(included_in: (1..6))
+      def accept visitor
+        visitor.process_add_move_card self
+      end
     end
 
     class UseMoveCard
       extend Dry::Initializer
       option :player,      type: Dry::Types['strict.hash']
       option :card_to_use, type: Dry::Types['strict.integer'].constrained(included_in: (1..6))
+      def accept visitor
+        visitor.process_use_move_card self
+      end
     end
 
     class DrawTopMoveCard
+      def accept visitor
+        visitor.process_draw_top_move_card self
+      end
     end
 
     class PlaceCardAtBottomOfMoveCardDeck
       extend Dry::Initializer
       option :card, type: Dry::Types['strict.integer'].constrained(included_in: (1..6))
+      def accept visitor
+        visitor.process_place_move_card_at_bottom_of_deck self
+      end
     end
 
     class DrawTopFreeGiftCard
+      def accept visitor
+        visitor.process_draw_top_move_card self
+      end
     end
 
     class PlaceCardAtBottomOfFreeGiftCardDeck
@@ -259,24 +280,36 @@ module SpyAlleyApplication
 
       extend Dry::Initializer
       option :card, type: Dry::Types['strict.string'].constrained(included_in: all_cards)
+      def accept visitor
+        visitor.process_place_card_at_bottom_of_free_gift_card_deck self
+      end
     end
 
     class MoveAction
       extend Dry::Initializer
       option :player,        type: Dry::Types['strict.hash']
       option :space_to_move, type: Dry::Types['strict.integer'].constrained(included_in: (0..32))
+      def accept visitor
+        visitor.process_move_action self
+      end
     end
 
     class AddMoney
       extend Dry::Initializer
       option :player, type: Dry::Types['strict.hash']
       option :amount, type: Dry::Types['strict.integer'].constrained(gt: 0)
+      def accept visitor
+        visitor.process_add_money self
+      end
     end
 
     class SubtractMoney
       extend Dry::Initializer
       option :player, type: Dry::Types['strict.hash']
       option :amount, type: Dry::Types['strict.integer'].constrained(gt: 0)
+      def accept visitor
+        visitor.process_subtract_money self
+      end
     end
 
     class AddEquipment
@@ -288,6 +321,9 @@ module SpyAlleyApplication
 
       option :player,    type: Dry::Types['strict.hash']
       option :equipment, type: Dry::Types['strict.string'].constrained(included_in: all_equipment)
+      def accept visitor
+        visitor.process_add_equipment self
+      end
     end
 
     class SubtractEquipment
@@ -300,42 +336,66 @@ module SpyAlleyApplication
 
       option :player,    type: Dry::Types['strict.hash']
       option :equipment, type: Dry::Types['strict.string'].constrained(included_in: all_equipment)
+      def accept visitor
+        visitor.process_subtract_equipment self
+      end
     end
 
     class EliminatePlayer
       extend Dry::Initializer
       option :player, type: Dry::Types['strict.hash']
+      def accept visitor
+        visitor.process_eliminate_player self
+      end
     end
 
     class IncorrectFreeGuess
       extend Dry::Initializer
       option :player_accused, type: Dry::Types['strict.hash']
+      def accept visitor
+        visitor.process_incorrect_free_guess self
+      end
     end
 
     class AddWildCard
       extend Dry::Initializer
       option :player, type: Dry::Types['strict.hash']
+      def accept visitor
+        visitor.accept_add_wild_card self
+      end
     end
 
     class SubtractWildCard
       extend Dry::Initializer
       option :player, type: Dry::Types['strict.hash']
+      def accept visitor
+        visitor.process_subtract_wild_card self
+      end
     end
 
     class ActionHashElement
       extend Dry::Initializer
       option :action_hash, type: Dry::Types['strict.hash']
+      def accept visitor
+        visitor.process_action_hash_element self
+      end
     end
 
     class NextActionOptions
       extend Dry::Initializer
       option :option, type: Dry::Types['strict.hash']
+      def accept visitor
+        visitor.process_next_action_options self
+      end
     end
 
     class GameVictory
       extend Dry::Initializer
       option :player, type: Dry::Types['strict.hash']
       option :reason, type: Dry::Types['strict.string']
+      def accept visitor
+        visitor.process_game_victory self
+      end
     end
 
     class SpyEliminatorOption
@@ -343,14 +403,23 @@ module SpyAlleyApplication
       option :options, type: Dry::Types['strict.array'].of(
         Dry::Types['strict.string'].constrained(included_in: (1..6).map{|seat| "seat_#{seat}"})
       )
+      def accept visitor
+        visitor.process_spy_eliminator_option self
+      end
     end
 
     class ConfiscateMaterialsOption
       extend Dry::Initializer
       option :options, type: Dry::Types['strict.hash']
+      def accept visitor
+        visitor.process_confiscate_materials_option self
+      end
     end
 
     class MoveBackTwoSpaces
+      def accept visitor
+        visitor.process_move_back_two_spaces self
+      end
     end
 
     class ChooseNewSpyIdentityOption
@@ -364,11 +433,17 @@ module SpyAlleyApplication
       option :remaining_choices, type: Dry::Types['strict.array'].of(
         Dry::Types['strict.integer'].constrained(included_in: (1..6))
       )
+      def accept visitor
+        visitor.process_choose_new_spy_identity_option self
+      end
     end
 
     class NextPlayerUp
       extend Dry::Initializer
       option :seat, type: Dry::Types['strict.integer'].constrained(included_in: (1..6))
+      def accept visitor
+        visitor.process_next_player_up
+      end
     end
 
     class TopLevelOptions
@@ -378,7 +453,9 @@ module SpyAlleyApplication
       option :accept_use_move_card, optional: true, type: Dry::Types['strict.array'].of(
         Dry::Types['strict.integer'].constrained(included_in: (1..6))
       )
+      def accept visitor
+        visitor.process_top_level_options self
+      end
     end
   end
 end
-
