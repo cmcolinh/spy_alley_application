f6b00cef0d2d438a2b471d03c4de49a8cf811540
Colin Horner <25807014+cmcolinh@users.noreply.github.com>
2021-08-20 22:23:59 +0900

    [Mod] Fixing handle use move card option node

--- lib/spy_alley_application/results/nodes/use_move_card_option_node.rb
+++ lib/spy_alley_application/results/nodes/use_move_card_option_node.rb
@@ -6,12 +6,12 @@ module SpyAlleyApplication
   module Results
     module Nodes
       class UseMoveCardOptionNode < Dry::Struct
-        @@can_handle_use_move_card = ::Types.Interface(:handle_use_move_card)
+        @@can_handle_use_move_card_option = ::Types.Interface(:handle_use_move_card_option)
         attribute :card_list, ::Types::ArrayOfStrictInteger
 
         def accept(visitor, **args)
-          @@can_handle_use_move_card.(visitor)
-          visitor.handle_use_move_card(self, args)
+          @@can_handle_use_move_card_option.(visitor)
+          visitor.handle_use_move_card_option(self, args)
         end
       end
     end
