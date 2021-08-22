e241742598fc50d0e05cb8ba2bf54ca2604fb0d9
Colin Horner <25807014+cmcolinh@users.noreply.github.com>
2021-08-22 15:58:31 +0900

    [Mod] Fix NewSpyIdentityChosen

--- lib/spy_alley_application/results/nodes/new_spy_identity_chosen_node.rb
+++ lib/spy_alley_application/results/nodes/new_spy_identity_chosen_node.rb
@@ -7,7 +7,7 @@ module SpyAlleyApplication
   module Results
     module Nodes
       class NewSpyIdentityChosenNode < Dry::Struct
-        @@can_handle_move_option = ::Types.Interface(:handle_new_spy_identity_chosen)
+        @@can_handle_new_spy_identity_chosen = ::Types.Interface(:handle_new_spy_identity_chosen)
         attribute :nationality, SpyAlleyApplication::Types::Nationality
 
         def accept(visitor, **args)
